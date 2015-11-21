9bf907211b664358c4c960272804440d05d9040e
Sam Spilsbury <smspillaz@gmail.com>
2015-11-22 00:36:23 +0800

    Add simple url override for google callback

--- oauth.js
+++ oauth.js
@@ -1,9 +1,11 @@
+var url_override = (process.env.URL_OVERRIDE || "http://serene-plains-5818.herokuapp.com");
+
 var ids = {
     google: {
-        callbackURL: "http://127.0.0.1:5000/champion-login/callback",
+        callbackURL: url_override + "/champion-login/callback",
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }
-}
+};
 
 module.exports = ids;
