8cf0ed0557a896b7d4a47cdd24b00d787a297d9a
Sam Spilsbury <smspillaz@gmail.com>
2015-11-21 15:03:35 +0800

    Start

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,4 +23,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/Post', function(req, res) {
+  console.log(req.param('name') + " " + req.param('project') + " " + req.param('url'));
+  res.redirect("/");
+});
